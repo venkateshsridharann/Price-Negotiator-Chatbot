784e63d1ced29643a5b935ba7232037d2fe6edfa
Venkatesh Sridharan <vs498@njit.edu>
2024-06-18 18:13:18 -0400

    Fixed Chat Model

--- negotiator/chat/views.py
+++ negotiator/chat/views.py
@@ -7,5 +7,5 @@ from .models import ChatSession
 def product_detail(request):
     # Filter chat sessions by the logged-in user
     chat_sessions = ChatSession.objects.filter(user=request.user)
-    return render(request, 'chat_interface.html', { 'chat_sessions': chat_sessions})
-
+    return render(request, 'chat_interface.html')
+    # return render(request, 'chat_interface.html', { 'chat_sessions': chat_sessions})
